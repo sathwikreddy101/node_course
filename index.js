ae02f396e96a612c976299afe44f520e5dc64fb4
Sathwik <sathwikreddy101@gmail.com>
2024-06-08 09:43:55 +0530

    integrating 3rd party module slugify into node project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
 
 const replaceTemplate = require('./modules/replaceTemplate');
 
@@ -42,6 +43,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`, 'utf-
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, {lower : true}));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
     const {query, pathname} = url.parse(req.url, true);
